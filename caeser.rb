7e15fb27857aae94da8f4daf3fdc78e222ec734b
Raza <owaisraza1704@gmail.com>
2022-08-31 10:58:59 +0530

    Solve the case and only letter change issue

--- caeser.rb
+++ caeser.rb
@@ -1,16 +1,23 @@
+# input text here
 text = 'abc efg'
+shift = 2
 split_text = text.split('')
+
   ord_conv=[]
 
+# fill letters to be converted
 split_text.each do |letter|
   ord_conv.push(letter.ord)
 end
 
 letter_shift = []
-shift = 2
 
 ord_conv.each do |ord|
-  letter_shift.push(ord+shift)
+    if ord.between?(65,90) || ord.between?(90,122)
+        letter_shift.push(ord+shift)
+      else
+        letter_shift.push(ord)
+      end
 end
 
 p letter_shift
@@ -25,3 +32,6 @@ p shifted_letters
 
 shifted_text = shifted_letters.join()
 p shifted_text
+
+
+# now current problems that needs to be tackled are - , starting from intial letters when no more letters ahead(for caps letter and small letters),
