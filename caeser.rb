b90fe2a8f0d8f16df7c9d57827d4826385d22786
Raza <owaisraza1704@gmail.com>
2022-08-31 11:34:53 +0530

    Complete Caeser cipher!

--- caeser.rb
+++ caeser.rb
@@ -1,34 +1,37 @@
-# input text here
-text = 'abc efg'
-shift = 2
+# testing spliting arrays
+p 'enter Text:'
+text = gets.chomp.to_s
+p 'enter how many letter shifts you want?'
+shift = gets.chomp.to_i
 split_text = text.split('')
+ord_conv = []
 
-  ord_conv=[]
-
-# fill letters to be converted
 split_text.each do |letter|
   ord_conv.push(letter.ord)
 end
+# p ord_conv
 
 letter_shift = []
 
 ord_conv.each do |ord|
-    if ord.between?(65,90) || ord.between?(90,122)
-        if ord + shift > 90 && ord <= 90
-            large_limit_cross = (ord + shift) - 90
-            letter_shift.push(64 + large_limit_cross)
-          elsif ord + shift > 122 && ord > 97
-            small_limit_cross = (ord + shift) - 122
-            letter_shift.push(96 + small_limit_cross)
-          else
-            letter_shift.push(ord + shift)
-          end
-      else
-        letter_shift.push(ord)
-      end
+  if ord.between?(65, 90) || ord.between?(90, 122)
+    if ord + shift > 90 && ord <= 90
+      large_limit_cross = (ord + shift) - 90
+      letter_shift.push(64 + large_limit_cross)
+    elsif ord + shift > 122 && ord > 97
+      small_limit_cross = (ord + shift) - 122
+      letter_shift.push(96 + small_limit_cross)
+    else
+      letter_shift.push(ord + shift)
+    end
+  # elsif ord == 10
+  #   next
+  else
+    letter_shift.push(ord)
+  end
 end
 
-p letter_shift
+# p letter_shift
 
 shifted_letters = []
 
@@ -36,8 +39,7 @@ letter_shift.each do |letter|
   shifted_letters.push(letter.chr)
 end
 
-p shifted_letters
+# p shifted_letters
 
-shifted_text = shifted_letters.join()
-p shifted_text
-
+shifted_text = shifted_letters.join
+p "Your Shifted Text is : #{shifted_text}"
